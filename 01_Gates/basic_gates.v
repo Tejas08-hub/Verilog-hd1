8ddde147c4a1c9412e32b41587ee88e2dc28a464
Tejas08-hub <tejasgowda07177@gmail.com>
2026-01-29 21:30:07 +0530

    Updated basic_gates module

--- 01_Gates/basic_gates.v
+++ 01_Gates/basic_gates.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 29.01.2026 19:53:00
-// Design Name: 
-// Module Name: and_gate
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module basic_gates(a,b,and1,or1,not1,nand1,nor1,xor1,xnor1);
 input a,b;
 output wire and1,or1,not1,nand1,nor1,xor1,xnor1;
